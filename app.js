db4f0474dc0fb7684cff637f8198284349a67ee7
Miyka96 <micaelaseminara@outlook.it>
2022-02-03 17:22:56 +0100

    first java lines

---
+++ app.js
@@ -0,0 +1,7 @@
+const userName = prompt ("Prego, inserisci pure il tuo nome");
+
+const userSurname = prompt ("Adesso gentilmente, inserisci il tuo cognome");
+
+const userColor = prompt ("Dimmi anche qual é il tuo colore preferito");
+
+const userPsw = (userName) + (userSurname) + (userColor) + 21;
